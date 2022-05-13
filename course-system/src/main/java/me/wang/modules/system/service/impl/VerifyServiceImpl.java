d3d9b59a2c3a7ce6e8f673dd0987086d666ac34d
wanglei110 <1151263480@qq.com>
2022-05-13 11:10:16 +0800

    提交

--- course-system/src/main/java/me/wang/modules/system/service/impl/VerifyServiceImpl.java
+++ course-system/src/main/java/me/wang/modules/system/service/impl/VerifyServiceImpl.java
@@ -34,32 +34,6 @@ public class VerifyServiceImpl implements VerifyService {
     private Long expiration;
     private final RedisUtils redisUtils;
 
-//    @Override
-//    @Transactional(rollbackFor = Exception.class)
-//    public EmailVo sendEmail(String email, String key) {
-//        EmailVo emailVo;
-//        String content;
-//        String redisKey = key + email;
-//        // 如果不存在有效的验证码，就创建一个新的
-//        TemplateEngine engine = TemplateUtil.createEngine(new TemplateConfig("template", TemplateConfig.ResourceMode.CLASSPATH));
-//        Template template = engine.getTemplate("email/email.ftl");
-//        Object oldCode =  redisUtils.get(redisKey);
-//        if(oldCode == null){
-//            String code = RandomUtil.randomNumbers (6);
-//            // 存入缓存
-//            if(!redisUtils.set(redisKey, code, expiration)){
-//                throw new BadRequestException("服务异常，请联系网站负责人");
-//            }
-//            content = template.render(Dict.create().set("code",code));
-//            emailVo = new EmailVo(Collections.singletonList(email),"EL-ADMIN后台管理系统",content);
-//        // 存在就再次发送原来的验证码
-//        } else {
-//            content = template.render(Dict.create().set("code",oldCode));
-//            emailVo = new EmailVo(Collections.singletonList(email),"EL-ADMIN后台管理系统",content);
-//        }
-//        return emailVo;
-//    }
-
     @Override
     public void validated(String key, String code) {
         Object value = redisUtils.get(key);
